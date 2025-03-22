dce5a12b5faaa73d6879c0f49388e0bce4dabfb4
syon <syon.xiii@gmail.com>
2025-03-22 14:28:44 +0900

    👕 ESLint Stylistic

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,40 @@
-// @ts-check
+import stylistic from '@stylistic/eslint-plugin'
+import simpleImportSort from 'eslint-plugin-simple-import-sort'
+import globals from 'globals'
+
 import withNuxt from './.nuxt/eslint.config.mjs'
 
 export default withNuxt(
-  // Your custom configs here
+  {
+    languageOptions: {
+      globals: globals.node,
+      parserOptions: {
+        ecmaVersion: 'latest',
+      },
+    },
+  },
+
+  {
+    plugins: {
+      'simple-import-sort': simpleImportSort,
+    },
+    rules: {
+      'simple-import-sort/exports': 'error',
+      'simple-import-sort/imports': 'error',
+    },
+  },
+
+  // https://eslint.style/guide/config-presets#configuration-factory
+  stylistic.configs.customize({
+    flat: true, // required for flat config
+    indent: 2,
+    quotes: 'single',
+    semi: false,
+  }),
+  {
+    plugins: {
+      '@stylistic': stylistic,
+    },
+    rules: {},
+  },
 )
