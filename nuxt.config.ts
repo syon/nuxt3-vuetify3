71ef345e628a8ad0e16a847d919f46126b454932
syon <syon.xiii@gmail.com>
2025-03-22 15:30:09 +0900

    ⚡ SPA

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: ['@nuxt/eslint'],
+
+  ssr: false,
+
   devtools: { enabled: true },
+
   compatibilityDate: '2024-11-01',
+
   eslint: {
     config: {
       stylistic: true,
