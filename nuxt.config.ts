ca26b0ef1cd5e81a604246c4340293e8fbc82514
syon <syon.xiii@gmail.com>
2025-03-23 14:19:54 +0900

    🔧 meta

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,8 +6,20 @@ export default defineNuxtConfig({
 
   devtools: { enabled: true },
 
+  // https://nuxt.com/docs/getting-started/seo-meta
   app: {
     baseURL: '/nuxt3-vuetify3/',
+    head: {
+      title: 'Nuxt3 + Vuetify3',
+      viewport: 'width=device-width, initial-scale=1',
+      meta: [
+        { name: 'description', content: 'My amazing site.' },
+        { name: 'format-detection', content: 'telephone=no' },
+      ],
+      htmlAttrs: {
+        lang: 'ja',
+      },
+    },
   },
 
   compatibilityDate: '2024-11-01',
